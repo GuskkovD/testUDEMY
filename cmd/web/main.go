2d2f0ee083580030d4364143fc5eeadcda4c2a38
dima <dimavg2001@gmail.com>
2021-09-13 15:00:09 +0300

    final version of preparation, all set, i will experiment with this a little bit, but im going to create a new repo for booking server

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,14 +5,28 @@ import (
 	"github.com/GuskkovD/go-course/pkg/config"
 	"github.com/GuskkovD/go-course/pkg/handlers"
 	"github.com/GuskkovD/go-course/pkg/render"
+	"github.com/alexedwards/scs/v2"
 	"log"
 	"net/http"
+	"time"
 )
 
 const portNumber = ":8080"
 
+var app config.AppConfig
+var session *scs.SessionManager
+
 func main() {
-	var app config.AppConfig
+
+	//change this to true while in production
+	app.InProduction = false
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,15 +3,26 @@ package main
 import (
 	"github.com/GuskkovD/go-course/pkg/config"
 	"github.com/GuskkovD/go-course/pkg/handlers"
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"net/http"
 )
 
 //routes sets multiplexers
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	//mux := pat.New()
+	//
+	//mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	//mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux := chi.NewRouter()
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+	mux.Use(SessionLoad)
+	//mux.Use(WriteToConsole)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+
 	return mux
 }
